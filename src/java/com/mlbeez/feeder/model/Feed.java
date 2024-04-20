f2737002f8fa54766a9ef0521fc95c21e8f180a1
Arivazhahan <114591821+arivu987@users.noreply.github.com>
2024-04-20 21:41:14 +0530

    Add files via upload

---
+++ src/java/com/mlbeez/feeder/model/Feed.java
@@ -0,0 +1,82 @@
+package com.mlbeez.feeder.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "feeds")
+public class Feed {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO) // Use GenerationType.IDENTITY for auto-generated IDs
+	@Column(columnDefinition = "bigint")
+	private Long id;
+
+	private String title;
+	
+	@JsonInclude(JsonInclude.Include.ALWAYS)
+	private String link;
+	
+	private String description;
+	private String author;
+	private String category;
+	private String tags;
+	
+	
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getLink() {
+		return link;
+	}
+
+	public void setLink(String link) {
+		this.link = link;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getTags() {
+		return tags;
+	}
+
+	public void setTags(String tags) {
+		this.tags = tags;
+	}
+}
